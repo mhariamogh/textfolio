dcaa6a90d99faedefdc8e3257b4487e686a428fc
hari <hmurugan@depaul.edu>
2024-09-29 01:38:18 -0700

    New files

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const readMoreLink = document.querySelector('.read-more');
+    const showLessLink = document.querySelector('.paragraph.hidden a');
+    const hiddenParagraph = document.querySelector('.paragraph.hidden');
+
+    if (readMoreLink && hiddenParagraph && showLessLink) {
+        readMoreLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            hiddenParagraph.classList.remove('hidden');
+            hiddenParagraph.classList.add('visible');
+            readMoreLink.classList.add('hidden');
+        });
+
+        showLessLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            hiddenParagraph.classList.remove('visible');
+            hiddenParagraph.classList.add('hidden');
+            
+            // Remove the 'hidden' class from the "Read More" link immediately
+            readMoreLink.classList.remove('hidden');
+        });
+    }
+});
