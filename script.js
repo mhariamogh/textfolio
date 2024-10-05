23d5571665f28e1aa42cbf7e53b631710f8e913a
hari <hmurugan@depaul.edu>
2024-10-04 22:42:13 -0700

    Fixed video and gif for mobile, added custom cursor

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
+
     const readMoreLink = document.querySelector('.read-more');
     const showLessLink = document.querySelector('.paragraph.hidden a');
     const hiddenParagraph = document.querySelector('.paragraph.hidden');
     const fadeInElements = document.querySelectorAll('.fade-in');
-    const video = document.getElementById('bio-video'); // Get the video element
+    const video = document.getElementById('bio-video');
+    const bioVideo = document.getElementById('bio-video');
+    const customCursor = document.createElement('div');
+    customCursor.classList.add('custom-cursor');
+    document.body.appendChild(customCursor);
+
+    bioVideo.addEventListener('mouseover', () => {
+        customCursor.style.opacity = 1; // Show the custom cursor
+    });
+
+    bioVideo.addEventListener('mouseout', () => {
+        customCursor.style.opacity = 0; // Hide the custom cursor
+    });
+
+    document.addEventListener('mousemove', (e) => {
+        customCursor.style.left = e.clientX + 'px';
+        customCursor.style.top = e.clientY + 'px';
+    });
 
 
     fadeInElements.forEach(element => {
