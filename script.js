d9e2616de6e3fb830eb9f7d2f64198fcfe646e36
hari <hmurugan@depaul.edu>
2024-10-04 21:58:21 -0700

    Added gif, icons to links, fixed padding and more

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const readMoreLink = document.querySelector('.read-more');
     const showLessLink = document.querySelector('.paragraph.hidden a');
     const hiddenParagraph = document.querySelector('.paragraph.hidden');
+    const fadeInElements = document.querySelectorAll('.fade-in');
+    const video = document.getElementById('bio-video'); // Get the video element
+
+
+    fadeInElements.forEach(element => {
+        setTimeout(() => {
+            element.classList.add('visible');
+        }, 20); // Adjust delay as needed
+    });
+
+    if (video) {
+        video.addEventListener('click', function () {
+            if (video.paused) {
+                video.play();
+            } else {
+                video.pause();
+                video.currentTime = 0; // Reset to the beginning if paused
+            }
+        });
+    }
 
     if (readMoreLink && hiddenParagraph && showLessLink) {
         readMoreLink.addEventListener('click', function (event) {
